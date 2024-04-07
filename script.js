c31fe42e8e2bf36df5eedb20edc747a465783f67
rdep1124 <143754743+rdep1124@users.noreply.github.com>
2024-04-07 16:34:50 -0400

    restricting the urge to drop out

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 const text_change = [
-  'This is the first graphic!',
-  'A second graphic!',
-  'A Third one!',
-  'Fourth, and one more on the way...',
-  'Last Graphic!',
-];
-
-const images = [
-  'media/items-on-shelves.jpeg',
-  'media/person-standing-at-shop.jpeg',
-  'media/colorful-bags.jpeg',
+  'THE HARDSHIPS OF CONGOLESE MIGRANTS',
+  'FINDING COMMUNITY IN HARLEM',
+  'A TASTE OF HOME'
 ];
 
 const updateContent = (index) => {
   // Update content based on the current index
-  // For example, update a text container with text_change[index]
-  // document.getElementById('text-container').innerText = text_change[index];
   console.log(text_change[index]);
 
   // Update image based on the current index
@@ -24,37 +14,45 @@ const updateContent = (index) => {
     document.getElementById('person'),
     document.getElementById('bags')
   ];
-  imageElements.forEach((imageElement, i) => {
-    if (i === index) {
-      imageElement.style.display = 'block'; // Show the image
-    } else {
-      imageElement.style.display = 'none'; // Hide other images
-    }
-  });
-};
 
+  // imageElements.forEach((imageElement, i) => {
+  //   if (i === index) {
+  //     imageElement.style.display = 'block'; // Show the image
+  //   } else {
+  //     imageElement.style.display = 'none'; // Hide other images
+  //   }
+  };
+
+const images = document.querySelectorAll('.image-container img');
 
 const scroller = new Scroller({
-  scene: document.querySelectorAll('.slide'), // Call it whatever but this is the class we use in html
+  scenes: document.querySelectorAll('.scene'),
   container: document.querySelector('.container'),
+  rootMargin: '-850px 0px', 
+  threshold: 0 
 });
 
-scroller.on('scene:enter', (d) => {
+// Scroller has a tiny event emitter embedded in it!
+
+// the `enter` event is triggered every time a scene crosses the threshold
+scroller.on('scene:enter', d => {
   // Add an active class to the div when it crosses in
   d.element.classList.add('active');    
-  // Change the content when a new scene enters
-  updateContent(d.index);
+  // change the text in the graphc container when it comes in
+  updateContent(d.index); // Call updateContent with the index
 });
 
-scroller.on('scene:exit', (d) => {
-  // Remove active class when it leaves 
+// the `exit` event is triggered every time a scene exits the threshold
+scroller.on('scene:exit', d => {
+  // remove active class when it leaves 
   d.element.classList.remove('active');
 });
 
+
+//don't worry about the stuff below here, but make sure to put it in at the end --------------------
+
 scroller.on('init', () => {
-  // Initializes the scroller
-  console.log('Scroller initialized!');
+  console.log('Everything is ready to go!');
 });
 
-// Starts up the IntersectionObserver
 scroller.init();
